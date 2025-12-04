e1fdd49ac154068b1ecca969526bfb7d46d8db8b
Erasmo Hernández <erasmoh@gmail.com>
2025-12-04 18:41:37 -0500

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "xynshcnkxdliapebmyaz.supabase.co",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
